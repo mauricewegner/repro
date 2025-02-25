7afbb94c1015e049ef833f84f6be469e2a0dba61
mauricewegner <mauricewegner@icloud.com>
2025-02-25 19:10:24 +0100

    fix: remove express type declarations

--- types/declarations/express.ts
+++
@@ -1,9 +0,0 @@
-import { RateLimitInfo } from 'express-rate-limit'
-import { SlowDownInfo } from 'express-slow-down'
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    rateLimit?: RateLimitInfo
-    slowDown?: SlowDownInfo
-  }
-}
